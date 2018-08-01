58727af35250a8c93a18ec2befd49aff5642bd4e
Ryan <ryan.elr@outlook.com>
2018-08-01 10:31:09 +0200

    newVersion

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,26 @@ $(document).ready(function() {
   $('.js-scrollTo').on('click', function() {
   var page = $(this).attr('href');
   var speed = 1800;
-  $('html, body').animate( { scrollBottom: $(page).offset().bottom}, speed );Go
-  return true;
- });
+  $('html, body').animate( { scrollTop: $(page).offset().top}, speed );//Go
+  return false;
 });
-$(function (){
-    function timer(n) {
-        $(".progress-bar").css("width", n + "%");
-        if(){
-              setTimeout(function() {
-                  timer(n + 10);
-            }, 200);
-        }
-    }
+  $('.js-scrollBottom').click(function(){
+    var doc = $(this).attr('href');
+    var slow = 1800;
+      $('html, body').animate({scrollTop:$(doc).offset().top}, slow);
+      return true;
+});
+});
+ /*$(function (){
+   function timer(n) {
+     $(".progress-bar").css("width", n + "%");
+     if(){
+       setTimeout(function() {
+         timer(n + 10);
+       }, 200);
+     }
+   }
 
-    timer(0);
-});
+   timer(0);
+ });
+ */
